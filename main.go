4bb8e0ab7fe4f37da7dcee3df2ffdc92020aefd4
ushan senarathna <ushansenarathna000@gmail.com>
2025-07-30 15:51:21 +0530

    feat: implement updating files by replacing

--- main.go
+++ main.go
@@ -29,8 +29,38 @@ func SaveData1(path string, data []byte) error {
 
 }
 
+// Saves data to a file by writing it to a temporary file first and then
+// renaming it to the final path.
+func SaveData2(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+	_, err = fp.Write(data)
+	if err != nil {
+		return err
+	}
+	err = fp.Sync() // fsync
+	if err != nil {
+		return err
+	}
+	return os.Rename(tmp, path)
+}
+
+func randomInt() int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int()
+}
 
 func main() {
 	SaveData1("test.txt", []byte("Hello, World!"))
+	SaveData2("test1.txt", []byte("Hello, World!"))
 
 }
