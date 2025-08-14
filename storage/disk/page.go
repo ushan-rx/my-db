65e72bdd7b52a0c872355b3f386626f765835d69
ushan senarathna <ushansenarathna000@gmail.com>
2025-08-14 17:08:16 +0530

    fix: Update Serialize method to return error for improved error handling

--- storage/disk/page.go
+++ storage/disk/page.go
@@ -7,6 +7,6 @@ type Page interface {
 	SetId(id int32)                // SetId sets unique identifier of the page.
 	Data() []byte                  // Data returns the data stored in the page.s
 	SetData(data []byte)           // SetData sets the data stored in the page.
-	Serialize() []byte             // Serialize serializes the page to a byte slice.
+	SSerialize() ([]byte, error)   // Serialize serializes the page to a byte slice.
 	Deserialize(data []byte) error // Deserialize deserializes the page from a byte slice.
 }
